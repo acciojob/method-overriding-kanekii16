fca12b844ae831d5747e15fb022939db3fbb484e
kanekii16 <iamsai1616@gmail.com>
2023-06-10 05:32:30 +0530

    Assignment to understand the concept of method overriding in Java : Solution

---
+++ src/main/java/com/driver/B.java
@@ -0,0 +1,9 @@
+package com.driver;
+
+public class B extends A{
+
+    @Override
+    public void meth() {
+        System.out.println("Method is overridden in Extendend class B");
+    }
+}
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+
+        B person = new B();
+
+        person.meth();
+
+    }
 }
+
+/*
+* When there was no method in child class the method war being called from parent class A itself.
+* but when the method was being written in child class too it just overridden the class A meth.
+* */
