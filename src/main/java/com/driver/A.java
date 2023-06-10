15c0422cc883644d7a4a54b6f98801e27e374d5f
kanekii16 <iamsai1616@gmail.com>
2023-06-11 03:55:05 +0530

    Solution

--- src/main/java/com/driver/A.java
+++ src/main/java/com/driver/A.java
@@ -1,7 +1,5 @@
 package com.driver;
 
 public class A {
-        public void meth(){
-            System.out.println("Invoking Method From Class A ");
-        }
+
 }
--- src/main/java/com/driver/B.java
+++ src/main/java/com/driver/B.java
@@ -2,8 +2,4 @@ package com.driver;
 
 public class B extends A{
 
-    @Override
-    public void meth() {
-        System.out.println("Method is overridden in Extendend class B");
-    }
 }
